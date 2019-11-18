6d610aaee1b20e02a581743a207747edf61901cf
bkm009 <bhavesh.kumar@bdb.ai>
2019-11-18 18:55:55 +0530

    Add jaavscript html django part one complete

---
+++ My_Django_Stuff/ProTwo/AppTwo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<em>My second App </em>")
+
+def help(request):
+    return render(request,"help/help.html",context=None)
